637479bb8e3ee958eab6370a0b742be4d425b7d2
Uziel Cordeiro <103205221+uzielcordeiro@users.noreply.github.com>
2024-05-21 22:58:43 -0300

    update code

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,24 @@
+describe('Orange HRM tests', () => {
+  it('Login - Sucess', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    //vamos substituir todos os seletores por únicos e menores
+
+    //cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get("[name='username']").type('Admin')
+    cy.get("[name='password']").type('admin123')
+    cy.get("[type='submit']").click()
+    cy.location('pathname').should('equal','/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+  })
+
+  it('Login - Fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('test')
+    cy.get("[name='password']").type('test')
+    cy.get("[type='submit']").click()
+    cy.get("[role='alert']")
+  
+  })
+})
+
+
