31bc8dad770f1a82ec796281ca769f3eac86fc14
Uziel Cordeiro <103205221+uzielcordeiro@users.noreply.github.com>
2024-05-22 23:32:25 -0300

    added selectors list for login specs

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,22 +1,28 @@
 describe('Orange HRM tests', () => {
+    const selectorList = {
+      usernameField: "[name='username']",
+      passwordField: "[name='password']",
+      loginButton:   "[type='submit']",
+      sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
+      wrongCredentialAlert: "[role='alert']"
+    }
+
   it('Login - Sucess', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    //vamos substituir todos os seletores por únicos e menores
-
-    //cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
-    cy.get("[name='username']").type('Admin')
-    cy.get("[name='password']").type('admin123')
-    cy.get("[type='submit']").click()
+   
+    cy.get(selectorList.usernameField).type('Admin')
+    cy.get(selectorList.passwordField).type('admin123')
+    cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
-    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+    cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
   })
 
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('test')
-    cy.get("[name='password']").type('test')
-    cy.get("[type='submit']").click()
-    cy.get("[role='alert']")
+    cy.get(selectorList.usernameField).type('test')
+    cy.get(selectorList.passwordField).type('test')
+    cy.get(selectorList.loginButton).click()
+    cy.get(selectorList.wrongCredentialAlert)
   
   })
 })
